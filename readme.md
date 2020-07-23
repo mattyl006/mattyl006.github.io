641dec82ba1c96f6d8d46f38df8daf5aef32c508
Mateusz <wirus006@gmail.com>
2020-07-23 02:13:40 +0200

    refactoring completed

--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
-# Portfolio website
+![cover](https://wirus006.github.io/og.png)
+
+# Mateusz Tylka - Computer Science Student
+
+This is my portfolio website.
+
+## Tools
+
+* WTF Webpack starterkit
+* HTML, CSS and JS
+* Github API
+
+## NPM Scripts
+
+`npm run start` - runs development mode
+
+`npm run build` - runs build process for production
+
+`npm run publish` - runs build process and publish the page using gh-pages branch
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
 // To fetch api:
 // repository name
 // repo describe
@@ -18,10 +12,6 @@ fetch('https://api.github.com/users/Wirus006/repos?sort=created')
         for(let repo of resp) {
             const {name, description, html_url, homepage} = repo;
             const repositoryList = document.querySelector('.projects__grid--js');
-            if(description == null) {
-                description = "Brak opisu."
-            }
-
             let myTemplate = `
             <article class="project">
                         <div class="project__bar">
@@ -43,14 +33,14 @@ fetch('https://api.github.com/users/Wirus006/repos?sort=created')
                             <p class="project__grid">
                                 <span class="project__label">demo:</span>
                                 <span
-                                    >&lt;<a class="project__link" href="${homepage}" title="hydrapp - demo">see here</a
+                                    >&lt;<a target="_blank" rel="noopener noreferrer" class="project__link" href="${homepage}" title="${name} - demo">see here</a
                                     >&gt;</span
                                 >
                             </p>
                             <p class="project__grid">
                                 <span class="project__label">github:</span>
                                 <span
-                                    >&lt;<a class="project__link" href="${html_url}" title="hydrapp - code"
+                                    >&lt;<a target="_blank" rel="noopener noreferrer" class="project__link" href="${html_url}" title="${name} - code"
                                         >source code</a
                                     >&gt;</span
                                 >
@@ -58,43 +48,11 @@ fetch('https://api.github.com/users/Wirus006/repos?sort=created')
                         </div>
                     </article>
             `;
-            repositoryList.innerHTML += myTemplate; 
+            if(homepage) {
+              repositoryList.innerHTML += myTemplate;
+            } 
         }
     })
     .catch(error => {
         console.log(`nie udało się pobrać danych: ${error}`);
     })
-
-document.addEventListener("DOMContentLoaded", () => {
-    //  little hack to detect if the user is on ie 11
-    const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-    // get all the links with an ID that starts with 'sectionLink'
-    const listOfLinks = document.querySelectorAll("a[href^='#sectionLink");
-    // loop over all the links
-    listOfLinks.forEach(function (link) {
-      // listen for a click
-      link.addEventListener('click',  () => {
-        // toggle highlight on and off when we click a link
-        listOfLinks.forEach( (link) => {
-          if (link.classList.contains('highlighted')) {
-            link.classList.remove('highlighted');
-          }
-         });
-        link.classList.add('highlighted');
-        // get the element where to scroll
-        let ref = link.href.split('#sectionLink');
-        ref = "#section" + ref[1];
-        // ie 11 does not support smooth scroll, so we will simply scroll
-        if (isIE11) {
-            window.scrollTo(0, document.querySelector(ref).offsetTop);
-        } else {
-            window.scroll({
-            behavior: 'smooth',
-            left: 0,
-            // top gets the distance from the top of the page of our target element
-            top: document.querySelector(ref).offsetTop
-            });
-        }
-        })
-    })
-})
